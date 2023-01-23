5e8c3013e339cdb2f55a95b3878ff43952927243
neni5 <62072219+neni5@users.noreply.github.com>
2023-01-23 23:34:53 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ class Main {
 	public static void main(String args[]) {
 		int i = 0;
 		System.out.println("the value of i is : " + i);
+		String name = "derek";
+		System.out.println("entered name is : " + name);
 	}
 }
